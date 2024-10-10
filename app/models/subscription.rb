411f23a2db1fd5297db23a36f80016e11664c2b3
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-10-10 10:52:40 -0400

    Ensure Subscriptions Aren't for A Negative Value

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.00 }
 
   enum :price_type, %i[ monthly annually ]
 
